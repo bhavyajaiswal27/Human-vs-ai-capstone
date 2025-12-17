69b2d3cbf6010fc703224e8d1524f9a1b458e80d
Bhavya Jaiswal <141169363+bhavyajaiswal27@users.noreply.github.com>
2025-12-17 23:31:56 +0530

    Simplify URL patterns by removing names

--- detector/urls.py
+++ detector/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import home, detect_text
 
 urlpatterns = [
-    path("", home, name="home"),                # fast page load
-    path("api/detect/", detect_text, name="detect_text"),
+    path("", home),
+    path("api/detect/", detect_text),
 ]
