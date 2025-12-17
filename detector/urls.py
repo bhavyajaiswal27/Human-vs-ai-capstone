d7761f0d70ba199e1b621e825b4e9625e64b4dea
Bhavya Jaiswal <141169363+bhavyajaiswal27@users.noreply.github.com>
2025-12-17 23:36:30 +0530

    Add names to URL patterns for clarity

--- detector/urls.py
+++ detector/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import home, detect_text
 
 urlpatterns = [
-    path("", home),
-    path("api/detect/", detect_text),
+    path("", home, name="home"),
+    path("api/detect/", detect_text, name="detect_text"),
 ]
